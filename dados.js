5a316b4b7e48d91672c06e6149088023760667e7
Britto-prog <rbritto2811@gmail.com>
2024-09-06 17:29:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Raimundos",
+      descricao: "Os Raimundos, banda de rock formada em Brasília em 1987, marcaram a cena musical brasileira com sua sonoridade única, que mesclava o rock clássico com elementos da cultura popular brasileira",
+      link: "https://pt.wikipedia.org/wiki/Raimundos",
+      tags: "raíz popular sertão "
+    },
+    {
+      titulo: "Sepultura",
+      descricao: "O Sepultura é uma das bandas de metal mais influentes do Brasil, conhecida por sua mistura de thrash metal com elementos de música tribal e indígena. Fundada em Belo Horizonte, a banda ganhou destaque internacional na década de 1990.",
+      link: "https://pt.wikipedia.org/wiki/Sepultura_(banda)",
+      tags: "nacional metal musica"
+    },
+    {
+      titulo: "Matanza",
+      descricao: "A Matanza é uma banda brasileira de rock que se destaca por suas letras engajadas e sonoridade pesada, com influências do hardcore punk e do metal. A banda é conhecida por suas performances enérgicas e por abordar temas sociais em suas músicas.",
+      link: "https://pt.wikipedia.org/wiki/Matanza_(banda)",
+      tags: "countrycore social iconico" 
+    }
+  ];
